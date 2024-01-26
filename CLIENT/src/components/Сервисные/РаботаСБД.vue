aa64efff65c2c7fa6031f4522bf34bd50c32dcdf
KDV <kovalevdmv@yandex.ru>
2024-01-27 01:53:22 +0300

    fix

--- CLIENT/src/components/Сервисные/РаботаСБД.vue
+++ CLIENT/src/components/Сервисные/РаботаСБД.vue
@@ -10,34 +10,61 @@
             </p>
         </Dialog>
 
-
-        <div style="display: flex; flex-direction: column; gap: 10px; padding: 50px;">
-            <dev style="display:grid; grid-template-columns: 1fr 7fr; justify-items: left; width: 100%;">
-                <div>Запрос:</div>
-                <textarea  v-model="Данные.Запрос" style="width: 100%;  box-shadow: 0px 0px 5px  rgba(0, 0, 0, 0.3); border-radius: 3px;" />
-                <div>Показывать ошибку в диалоге:</div>
-                <input type="checkbox" v-model="Данные.ПоказыватьОшибкуВДиалоге" />
-            </dev>
-            <v-btn @click="ОбновитьДанные">Выполнить</v-btn>
-            <DataTable columnResizeMode='fit' scrollable scrollHeight='700px' selectionMode='single' stripedRows
-                size='small' :value='Данные.РезультатЗапроса' tableStyle='min-width: 50rem'
-                @row-dblclick='ДвойнойКликПоСтрокеТаблицы' :resizable-columns='true' sortField='id' :sortOrder='1'
-                showGridlines style="user-select: none;">
-                <Column v-for='(col, index) in Данные.Колонки' :key='index' :field='col' :header='col'
-                    :sortable='true'>
-                </Column>
-            </DataTable>
-        </div>
+        <TabView>
+            <TabPanel header="Запрос">
+                <div style="display: flex; flex-direction: column; gap: 10px; padding: 50px;">
+                    <dev style="display:grid; grid-template-columns: 1fr 7fr; justify-items: left; width: 100%;">
+                        <div>Запрос:</div>
+                        <textarea v-model="Данные.Запрос"
+                            style="width: 100%;  box-shadow: 0px 0px 5px  rgba(0, 0, 0, 0.3); border-radius: 3px;" />
+                        <div>Показывать ошибку в диалоге:</div>
+                        <input type="checkbox" v-model="Данные.ПоказыватьОшибкуВДиалоге" />
+                    </dev>
+                    <v-btn @click="ОбновитьДанные">Выполнить</v-btn>
+                    <DataTable columnResizeMode='fit' scrollable scrollHeight='700px' selectionMode='single' stripedRows
+                        size='small' :value='Данные.РезультатЗапроса' tableStyle='min-width: 50rem'
+                        @row-dblclick='ДвойнойКликПоСтрокеТаблицы' :resizable-columns='true' sortField='id' :sortOrder='1'
+                        showGridlines style="user-select: none;">
+                        <Column v-for='(col, index) in Данные.Колонки' :key='index' :field='col' :header='col'
+                            :sortable='true'>
+                        </Column>
+                    </DataTable>
+                </div>
+            </TabPanel>
+            <TabPanel header="Создать таблицы базы данных">
+                <v-btn @click="ОбновитьТаблицыВБД">Обновить таблицы в БД</v-btn>
+                <div>
+                    <div v-for="(el, index) of Данные.СписокТаблиц" :key="index">
+                        <input type="checkbox" v-model="el.checked">
+                        {{ el.table.Имя }}
+                    </div>
+                </div>
+                <!--
+                <h2>Текст запроса</h2>
+                <div v-for="(el, index) of Данные.СписокТаблиц" :key="index" v-if="el.checked">
+                    <textarea>{{ el.table.Имя }}</textarea>
+                </div>
+                -->
+            </TabPanel>
+        </TabView>
 
     </div>
 </template>
 
 <style scope></style>
 
-<script setup>
+<script setup lang="ts">
 
-import { ref, inject } from 'vue';
+import { ref, inject, onMounted } from 'vue';
 import GeneralFunc from '@/GeneralFunc.js';
+import ТаблицыБД from '@/Настройки/ТаблицыБазыДанных';
+
+function ОбновитьТаблицыВБД() {
+    Данные.value.СписокТаблиц.filter(table => table.checked).forEach((table)=>{
+        Данные.value.Запрос = table.table.ТестЗапросаНаСоздание();
+        console.log(Данные.value.Запрос);
+    });
+}
 
 const Данные = ref(
     {
@@ -46,7 +73,8 @@ const Данные = ref(
         Колонки: [],
         ПоказатьДиалогСОшибкой: false,
         ОшибкаЗапроса: '',
-        ПоказыватьОшибкуВДиалоге: false
+        ПоказыватьОшибкуВДиалоге: false,
+        СписокТаблиц: []
     }
 );
 
@@ -68,5 +96,11 @@ async function ОбновитьДанные() {
     }
 }
 
+onMounted(() => {
+    Данные.value.СписокТаблиц = ТаблицыБД.map(table => ({
+        table: table,
+        checked: false
+    }));
+});
 
 </script>
